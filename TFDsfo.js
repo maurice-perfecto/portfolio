60dcc001fcb72de576cd9ad8cba864aaed798980
Maurice <mcruz03@unm.edu>
2018-02-14 17:04:07 -0700

    17:04
    17:04

--- TFDsfo.js
+++ TFDsfo.js
@@ -1,8 +1,8 @@
  var map;
-      function initMap() {
+      function initialize() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 42.936017, lng: -114.405433},
-          zoom: 8
+          zoom: 8,
             styles: [
   {
     "elementType": "geometry",
