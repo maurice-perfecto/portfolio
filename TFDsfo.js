7aeae4ed6b17b3d8b92a1dd8ff5bc6ee987e5a3e
Maurice <mcruz03@unm.edu>
2018-02-15 10:21:07 -0700

    10:21
    10:21

--- TFDsfo.js
+++ TFDsfo.js
@@ -1,8 +1,8 @@
 var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 42.936017, lng: -114.40543},
-          zoom: 9
-        });
+            center: {lat: 42.936017, lng: -114.40543},
+            zoom: 9,
+           });
       }
-                                  
+ 
