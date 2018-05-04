81305866dd720eb67727eb4bd4b36a9a29672d5f
Maurice <mcruz03@unm.edu>
2018-05-04 00:00:18 -0600

    22
    22

--- CustomizedOL.js
+++ CustomizedOL.js
@@ -2,25 +2,34 @@
 
 var MurphyComplex = new ol.layer.Vector({
 	source: new ol.source.Vector({
-		url: 'http://maurice-perfecto.github.io/portfolio/MurphyComplex.kml',
+		url: 'http://maurice-perfecto.github.io/portfolio/MurphyComplex.html',
 		format: new ol.format.KML()
 	})
-});
+})
+
+var TFDsfo = new ol.layer.Vector({
+	source: new ol.source.Vector({
+		url: 'http://maurice-perfecto.github.io/portfolio/SFO.html',
+		format: new ol.format.KML()
+	})
+})
+
+
+var basemap = new ol.layer.Tile({
+                    source: new ol.source.Stamen({
+                        layer: 'terrain'
+                    })
+                })
 
 
 
 var map = new ol.Map({
             target: 'map',
-            layers: [MurphyComplex,
-                new ol.layer.Tile({
-                    source: new ol.source.Stamen({
-                        layer: 'terrain'
-                    })
-                }),
+            layers: [basemap, MurphyComplex, TFDsfo,
 
             ],
             view: new ol.View({
                 center: ol.proj.fromLonLat([-115.368205, 42.206283]),
-                zoom: 10
+                zoom: 9
             })
         })
