47de483e5993a6e63a1c577e76b5502ecc94d4d9
Maurice <mcruz03@unm.edu>
2018-05-04 00:25:45 -0600

    qq
    qq

--- CustomizedOL.js
+++ CustomizedOL.js
@@ -1,16 +1,35 @@
 //Based on examples from OpenLayers and code from Stamen
+var fire_perim_color = [50,45,46,.25]
+var fire_fill_color = [181,52,60,1]
+
+var fire_style = new ol.style.Style({
+	fill: new ol.style.Fill({
+	  color: fire_fill_color
+	}),
+	stroke: new ol.style.Stroke({
+	  color: fire_perim_color,
+	  width: 2
+	}),
+});
 
 var MurphyComplex = new ol.layer.Vector({
 	source: new ol.source.Vector({
 		url: 'http://maurice-perfecto.github.io/portfolio/MurphyComplex.html',
-		format: new ol.format.KML()
-	})
+		format: new ol.format.KML({
+				extractStyles:false
+		})
+	}),
+	style: fire_style
 })
 
-var TFDsfo = new ol.layer.Vector({
-	source: new ol.source.Vector({
-		url: 'http://maurice-perfecto.github.io/portfolio/SFO.html',
-		format: new ol.format.KML()
+var GEOL = new ol.layer.Tile({
+	source: new ol.source.TileWMS({
+	url: 'https://mrdata.usgs.gov/services/id?version=1.3.0',
+	  params: {
+		LAYERS: 'Idaho_Geology',
+		FORMAT: 'image/png',
+		TRANSPARENT: true
+	  }
 	})
 })
 
@@ -25,7 +44,7 @@ var basemap = new ol.layer.Tile({
 
 var map = new ol.Map({
             target: 'map',
-            layers: [basemap, MurphyComplex, TFDsfo,
+            layers: [basemap, MurphyComplex,
 
             ],
             view: new ol.View({
