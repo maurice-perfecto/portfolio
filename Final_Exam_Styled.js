510ba0bf1f876a6744d5bb0c8159fab04e6c99df
Maurice <mcruz03@unm.edu>
2018-05-06 14:36:39 -0600

    33
    33

--- Final_Exam_Styled.js
+++ Final_Exam_Styled.js
@@ -1,33 +1,33 @@
 //WMS tiles from "Clipped" directory
 
 
-var Shape_1 = new ol.layer.Tile({
+var Styled_Shape_1 = new ol.layer.Tile({
 	source: new ol.source.TileWMS({
 		url: 'http://internetmapping.net:8080/geoserver/ws_mcruz03/ows?service=wms&version=1.3.0&request=GetCapabilities',
 		params: {
-			LAYERS: 'mc35106-A5_CONT',
+			LAYERS: '',
 			FORMAT: 'image/png',
 			TRANSPARENT: true
 		}
 	})
 })
 
-var Shape_2 = new ol.layer.Tile({
+var Styled_Shape_2 = new ol.layer.Tile({
 	source: new ol.source.TileWMS({
 		url: 'http://internetmapping.net:8080/geoserver/ws_mcruz03/ows?service=wms&version=1.3.0&request=GetCapabilities',
 		params: {
-			LAYERS: 'mc_35106-A6_CONT',
+			LAYERS: '',
 			FORMAT: 'image/png',
 			TRANSPARENT: true
 		}
 	})
 })
 
-var Shape_3 = new ol.layer.Tile({
+var Styled_Shape_3 = new ol.layer.Tile({
 	source: new ol.source.TileWMS({
 		url: 'http://internetmapping.net:8080/geoserver/ws_mcruz03/ows?service=wms&version=1.3.0&request=GetCapabilities',
 		params: {
-			LAYERS: 'mc_tgr2006se_bern_lka',
+			LAYERS: 'tiger',
 			FORMAT: 'image/png',
 			TRANSPARENT: true
 		}
@@ -62,7 +62,7 @@ var Tiff_2 = new ol.layer.Tile({
 
 var map = new ol.Map({
 	target: 'map',
-	layers: [Tiff_1, Tiff_2, Shape_1, Shape_2, Shape_3,
+	layers: [Tiff_1, Tiff_2, Styled_Shape_1, Styled_Shape_2, Styled_Shape_3
 					],
 	view: new ol.View({
 		center: ol.proj.fromLonLat([-106.6, 35.08]),
