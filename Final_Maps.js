49148d265484ebb60a7f418e01c5cdf035751363
Maurice <mcruz03@unm.edu>
2018-05-06 15:54:03 -0600

    22
    22

--- Final_Maps.js
+++ Final_Maps.js
@@ -1,93 +1,68 @@
 var MurphyComplex = new ol.layer.Tile({
 	source: new ol.source.TileWMS({
-	url: 'http://internetmapping.net:8080/geoserver/wms?',
-	  params: {
-		LAYERS: 'Murphy_TFD_2007',
-		FORMAT: 'image/png',
-		TRANSPARENT: true
-	  }
+		url: 'http://internetmapping.net:8080/geoserver/ws_mcruz03/ows?service=wms&version=1.3.0&request=GetCapabilities',
+		params: {
+			LAYERS: 'Murphy_TFD_2007',
+			FORMAT: 'image/png',
+			TRANSPARENT: true
+		}
 	})
 })
 
 var basemap = new ol.layer.Tile({
-                    source: new ol.source.Stamen({
-                        layer: 'terrain'
-                    })
-                })
+	source: new ol.source.Stamen({
+		layer: 'terrain'
+	})
+})
 var PostClassChange = new ol.layer.Tile({
 	source: new ol.source.TileWMS({
-	url: 'http://internetmapping.net:8080/geoserver/ws_mcruz03/ows?version=1.3.0',
-	  params: {
-		LAYERS: 'No_Change_Veg_Type',
-		FORMAT: 'image/png',
-		TRANSPARENT: true
-	  }
+		url: 'http://internetmapping.net:8080/geoserver/ws_mcruz03/ows?service=wms&version=1.3.0&request=GetCapabilities',
+		params: {
+			LAYERS: 'No_Change_Veg_Type',
+			FORMAT: 'image/png',
+			TRANSPARENT: true
+		}
 	})
 })
 
 var Veg2006 = new ol.layer.Image({
-					source: new ol.source.TileWMS({
-						url: 'http://internetmapping.net:8080/geoserver/ws_mcruz03/ows?version=1.3.0',
-						params: {
-							LAYERS: 'Class_Veg_2006',
-							FORMAT: 'image/png',
-							TRANSPARENT: true
-						}
-					})
+	source: new ol.source.TileWMS({
+		url: 'http://internetmapping.net:8080/geoserver/ws_mcruz03/ows?service=wms&version=1.3.0&request=GetCapabilities',
+		params: {
+			LAYERS: 'Class_Veg_2006',
+			FORMAT: 'image/png',
+			TRANSPARENT: true
+		}
+	})
 })
 
 var Veg2008 = new ol.layer.Image({
-					source: new ol.source.TileWMS({
-						url: 'http://internetmapping.net:8080/geoserver/ws_mcruz03/ows?version=1.3.0',
-						params: {
-							LAYERS: 'Class_Veg_2008',
-							FORMAT: 'image/png',
-							TRANSPARENT: true
-						}
-					})
+	source: new ol.source.TileWMS({
+		url: 'http://internetmapping.net:8080/geoserver/ws_mcruz03/ows?service=wms&version=1.3.0&request=GetCapabilities',
+		params: {
+			LAYERS: 'Class_Veg_2008',
+			FORMAT: 'image/png',
+			TRANSPARENT: true
+		}
+	})
 })
 
 ///////////////////////////////////////////////////////////////////////////////
 // create our base map objects 
 var map = new ol.Map({
-            target: 'map',
-            layers: [basemap, MurphyComplex,],
-            view: new ol.View({
-                center: ol.proj.fromLonLat([-115.368205, 42.206283]),
-                zoom: 9
-            })
-        })
+	target: 'map',
+	layers: [basemap, MurphyComplex, ],
+	view: new ol.View({
+		center: ol.proj.fromLonLat([-115.368205, 42.206283]),
+		zoom: 9
+	})
+})
 
 var VegMap = new ol.Map({
-            target: 'map',
-            layers: [Veg2006, Veg2008],
-			controls: ol.control.defaults({
-			attributionOptions:{
-				collaspible: false
-			}
-			}),
-            view: new ol.View({
-                center: ol.proj.fromLonLat([-115.368205, 42.206283]),
-                zoom: 8
-            })
-        });
-var swipe = document.getElementById('swipe');
-
-      Veg2006.on('precompose', function(event) {
-        var ctx = event.context;
-        var width = ctx.canvas.width * (swipe.value / 100);
-
-        ctx.save();
-        ctx.beginPath();
-        ctx.rect(width, 0, ctx.canvas.width - width, ctx.canvas.height);
-        ctx.clip();
-      });
-
-      Veg2006.on('postcompose', function(event) {
-        var ctx = event.context;
-        ctx.restore();
-      });
-
-      swipe.addEventListener('input', function() {
-        map.render();
-      }, false);
+	target: 'map',
+	layers: [Veg2006, Veg2008],
+	view: new ol.View({
+		center: ol.proj.fromLonLat([-115.64209, 42.55280]),
+		zoom: 5
+	})
+});
